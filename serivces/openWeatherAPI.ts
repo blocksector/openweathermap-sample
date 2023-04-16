22ed02bfe66312fd8026ec810b4bf36b3b7fa6dd
Jay Gauten <>
2023-04-16 12:50:56 +0800

    added env variables

--- serivces/openWeatherAPI.ts
+++ serivces/openWeatherAPI.ts
@@ -1,23 +1,24 @@
 
-const host = 'http://api.openweathermap.org/';
-const countryCode = 'PH'
-const appId = '';
+const host = process.env.WEATHER_API;
+const countryCode = process.env.COUNTRY_CODE
+const appId = process.env.WEATHER_API_KEY;
+
+const weatherEndpoint = `${host}data/2.5/weather`;
+const geolocEndpoint = `${host}geo/1.0/direct`;
 
 export const getLocations = async (location) => {
-    const url = `${host}geo/1.0/direct`
     const searchParams = new URLSearchParams({
         q: `${location},${countryCode}`,
         limit: 5,
         appid: appId,
     } as any)
 
-    return await fetch(`${url}?${searchParams}`, {
+    return await fetch(`${geolocEndpoint}?${searchParams}`, {
         "method": "GET",
     }).then((res) => res.json())
 }
 
 export const getLocationWeatherByCoords = async ({ lat, lon, units }) => {
-    const url = `${host}data/2.5/weather`
     const searchParams = new URLSearchParams({
         lat,
         lon,
@@ -25,20 +26,19 @@ export const getLocationWeatherByCoords = async ({ lat, lon, units }) => {
         appid: appId
     } as any)
 
-    return await fetch(`${url}?${searchParams}`, {
+    return await fetch(`${weatherEndpoint}?${searchParams}`, {
         "method": "GET",
     }).then((res) => res.json())
 }
 
 export const getLocationWeather = async ({ q, units, wforecast }) => {
-    const url = `${host}data/2.5/weather`
     const searchParams = new URLSearchParams({
         q: `${q},${countryCode}`,
         units,
         appid: appId
     } as any)
 
-    return await fetch(`${url}?${searchParams}`, {
+    return await fetch(`${weatherEndpoint}?${searchParams}`, {
         "method": "GET",
     }).then((res) => res.json())
 }
